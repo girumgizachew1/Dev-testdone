e4b33c4d9aa7792ec55606bf90650f43a3579a7b
unknown <giri@10000>
2022-12-27 01:46:56 +0300

    fix some errors

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bcd6141ba5a644f6"; }
+/******/ 		__webpack_require__.h = function() { return "80d0734b38656c80"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
